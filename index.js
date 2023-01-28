49c79ba2d200da6ab74265e106c52f925521da07
Ameen Abdulsamad <amenchawshly98@gmail.com>
2023-01-28 15:15:05 +0300

    assignment completed

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(line => {
+  let words = line.split(" ")
+  let capitalization = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  return capitalization.join(" ")
+})
